31c9c07fcfe000eabc4f1e83da751cdadf791d45
Fabio Pereira <fabiopereira.me@gmail.com>
2015-10-18 14:28:15 +1100

    installing blogpt for the first time

---
+++ install_blogs.sh
@@ -0,0 +1,13 @@
+mv index.html index-home.html
+cp -rf ../fabiopereirame_jekyll_mickey_blog/_site/ .
+mv index.html blog/
+mv index-home.html index.html
+rm -rf blog/wp-content
+mv wp-content blog
+
+
+rm -rf blogpt
+mkdir blogpt
+cp -rf ../fabiopereirame_jekyll_mickey_blogpt/_site/blogpt .
+cp     ../fabiopereirame_jekyll_mickey_blogpt/_site/index.html blogpt/
+cp -rf ../fabiopereirame_jekyll_mickey_blogpt/wp-content blogpt/
