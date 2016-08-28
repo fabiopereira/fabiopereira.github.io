75f0060288b630cf083a1dc42609f923d70ddb0d
Fabio Pereira <fabiopereira.me@gmail.com>
2016-08-28 13:22:21 -0300

    BINTECH folder

--- PeopleDataTimezones/Time Zones_files/data.js
+++ PeopleDataTimezones/Time Zones_files/data.js
@@ -1,1 +1,1 @@
-var data = [[-10.0,"Recife","BRT UTC-3",true],[1.0,"Manchester","BST UTC+1",true],[5.5,"Bangalore","IST UTC+5.5",true]];
+var data = [[5.5,"Bangalore","IST UTC+5.5",true],[1.0,"Manchester","BST UTC+1",true],[-10.0,"Recife","BRT UTC-3",true]];
