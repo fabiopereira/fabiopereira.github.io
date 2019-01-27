e8db33ab8d87471df80a62240d33613c60760825
Fabio Pereira <fabiopereira.me@gmail.com>
2019-01-27 12:09:50 -0200

    EveryTimeZoneRedHatTabata

---
+++ EveryTimeZone/EveryTimeZoneRedHatTabata/Time Zones_files/data.js
@@ -0,0 +1,1 @@
+var data = [[-8.0,"San Francisco","PST UTC-8",true],[-7.0,"Denver","PST UTC-7",true],[-5.0,"Boston","-05 UTC-5",true],[-2.0,"Sao Paulo","-02 UTC-2",true],[1.0,"Belgium","+01 UTC+1",true]];
