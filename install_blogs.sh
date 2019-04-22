d84aeec94232662fcc291e09d67963396a1c95c0
Fabio Pereira <fabiopereira.me@gmail.com>
2019-04-22 20:49:28 -0300

    palestras/psicogniagil

--- install_blogs.sh
+++ install_blogs.sh
@@ -2,6 +2,10 @@ cp -rf ../fabiopereirame_jekyll_mickey_blog/_site/index.html blog
 cp -rf ../fabiopereirame_jekyll_mickey_blog/_site/wp-content blog
 cp -rf ../fabiopereirame_jekyll_mickey_blog/_site/assets blog
 
+# Replace href="/assets/ by href="/blogpt/assets/
+
 cp -rf ../fabiopereirame_jekyll_mickey_blogpt/_site/index.html blogpt
 cp -rf ../fabiopereirame_jekyll_mickey_blogpt/_site/wp-content blogpt
 cp -rf ../fabiopereirame_jekyll_mickey_blogpt/_site/assets blogpt
+
+# Replace href="/assets/ by href="/blogpt/assets/
