0d0cbbea198281b387db74f75499e9d4c591c91f
Fabio Pereira <fabiopereira.me@gmail.com>
2019-04-22 20:38:20 -0300

    changing path for blogpt/assets

--- install_blogs.sh
+++ install_blogs.sh
@@ -1,15 +1,7 @@
-mv index.html index-home.html
-cp -rf ../fabiopereirame_jekyll_mickey_blog/_site/ .
-mv index.html blog/
-mv index-home.html index.html
-rm -rf blog/wp-content
-mv wp-content blog
+cp -rf ../fabiopereirame_jekyll_mickey_blog/_site/index.html blog
+cp -rf ../fabiopereirame_jekyll_mickey_blog/_site/wp-content blog
+cp -rf ../fabiopereirame_jekyll_mickey_blog/_site/assets blog
 
-
-rm -rf blogpt
-mkdir blogpt
-cp -rf ../fabiopereirame_jekyll_mickey_blogpt/_site/blogpt .
-cp     ../fabiopereirame_jekyll_mickey_blogpt/_site/index.html blogpt/
-cp -rf ../fabiopereirame_jekyll_mickey_blogpt/wp-content blogpt/
-cp -rf ../fabiopereirame_jekyll_mickey_blogpt/assets/images/hero/ assets/images/hero
-cp -rf ../fabiopereirame_jekyll_mickey_blogpt/assets/images/thumbnail/ assets/images/thumbnail
+cp -rf ../fabiopereirame_jekyll_mickey_blogpt/_site/index.html blogpt
+cp -rf ../fabiopereirame_jekyll_mickey_blogpt/_site/wp-content blogpt
+cp -rf ../fabiopereirame_jekyll_mickey_blogpt/_site/assets blogpt
